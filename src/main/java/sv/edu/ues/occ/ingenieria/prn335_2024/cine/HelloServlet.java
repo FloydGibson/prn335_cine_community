8f29384e273ff02d2fac4b4c16c76e4bb0e2ccbb
daoc7 <oc22002@ues.edu.sv>
2024-08-26 09:03:57 -0600

    branches establecidas

--- src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/HelloServlet.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/HelloServlet.java
@@ -1,5 +1,5 @@
 package sv.edu.ues.occ.ingenieria.prn335_2024.cine;
-//un saludo pibes, esta es la branch de clase
+//un saludo pibes, esta es la branch de master
 import java.io.*;
 
 import jakarta.servlet.http.*;

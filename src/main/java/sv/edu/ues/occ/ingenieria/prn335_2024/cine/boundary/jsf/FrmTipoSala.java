b27924e8a88646e283a1ba37b5cda68031542ad0
daoc7 <oc22002@ues.edu.sv>
2024-09-18 23:10:29 -0600

    Actualizacion

--- src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/FrmTipoSala.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/FrmTipoSala.java
@@ -1,6 +1,7 @@
 package sv.edu.ues.occ.ingenieria.prn335_2024.cine.boundary.jsf;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.event.ActionEvent;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -9,22 +10,27 @@ import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.TipoSala;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Named
 @ViewScoped
 public class FrmTipoSala implements Serializable {
-@Inject
-TipoSalaBean tsBean;
 
-private TipoSala tipoSala;
-List<TipoSala> registros;
+    @Inject
+    TipoSalaBean tsBean;
 
-@PostConstruct
-        public void init() {
-    registros = tsBean.findRange(0,1000);
+    List<TipoSala> registros;
 
-}
-TipoSala registro;
+    @PostConstruct //Solo lo puedo usar una vez por clase
+    public void init() {
+        registros = tsBean.findRange(0,10000000);
+    }
+
+    TipoSala registro;
+
+    public void btnSeleccionarRegistroHandler(ActionEvent ae){
+        System.out.println("Selecciono algo");
+    }
 
     public List<TipoSala> getRegistros() {
         return registros;
@@ -34,6 +40,17 @@ TipoSala registro;
         this.registros = registros;
     }
 
+    public Integer getSeleccionado(){
+        if(registro == null){
+            return null;
+        }
+        return registro.getIdTipoSala();
+    }
+
+    public void setSeleccionado(Integer seleccionado){
+        this.registro= this.registros.stream().filter(r->r.getIdTipoSala()==seleccionado).collect(Collectors.toList()).getFirst();
+    }
+
     public TipoSala getRegistro() {
         return registro;
     }
@@ -41,14 +58,6 @@ TipoSala registro;
     public void setRegistro(TipoSala registro) {
         this.registro = registro;
     }
-
-    String nombre="Chepe";
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
+
+

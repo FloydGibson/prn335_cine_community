8c0d249bf537e88f645e89f1dabf6ab1b13c0cfa
ms22050 <ms22050@ues.edu.sv>
2024-09-18 21:01:34 -0600

    agregue asientobean

--- src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/control/AsientoBean.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/control/AsientoBean.java
@@ -1,7 +1,8 @@
 package sv.edu.ues.occ.ingenieria.prn335_2024.cine.control;
+
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
-import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.Sala;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.Asiento;
 import jakarta.persistence.EntityManager;
 
 import java.io.Serializable;
@@ -11,39 +12,35 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class SalaBean extends AbstractDataPersist<Sala> implements Serializable {
+public class AsientoBean extends AbstractDataPersist<Asiento> implements Serializable {
 
     @PersistenceContext(unitName = "CinePU")
     EntityManager em;
 
-    public SalaBean() {
-        super(Sala.class);
+    public AsientoBean() {
+        super(Asiento.class);
     }
 
     @Override
     public EntityManager getEntityManager() {
-        return null;
+        return em;
     }
 
-
-    public List<Sala> findByIdTipoSala(Integer idTipoSala, int first, int max) {
-        if(idTipoSala!=null && first>=0 && max>0){
+    public List<Asiento> findByIdSala(Long idSala, int first, int max) {
+        if (idSala != null && first >= 0 && max > 0) {
             try {
-                if(em!=null){
-
-                    Query q = em.createNamedQuery("Sala.findByIdTipoSala");
-                q.setParameter("idTipoSala", idTipoSala);
-                q.setFirstResult(first);
-                q.setMaxResults(max);
+                if (em != null) {
+                    Query q = em.createNamedQuery("Asiento.findByIdSala");
+                    q.setParameter("idSala", idSala);
+                    q.setFirstResult(first);
+                    q.setMaxResults(max);
                     return q.getResultList();
                 }
+
             } catch (Exception e) {
                 Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
             }
-
-
         }
-
         return Collections.emptyList();
     }
 }

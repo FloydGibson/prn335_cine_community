06a78fa14ec4ea4915caedec424b93734eae5b89
daoc7 <oc22002@ues.edu.sv>
2024-10-04 21:05:27 -0600

    creacion de frm tipo reserva, enum de estad crud, falta implementacion de abstractFRM

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/FrmTipoReserva.java
@@ -0,0 +1,49 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.boundary.jsf;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortMeta;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.control.TipoReservaBean;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.TipoReserva;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@Named
+@SessionScoped
+public class FrmTipoReserva implements Serializable {
+@Inject
+TipoReservaBean dataBean;
+LazyDataModel<TipoReserva> modelo;
+
+@PostConstruct
+    public void inicializar(){
+    modelo=new LazyDataModel<TipoReserva>(){
+        @Override
+        public int count(Map<String, FilterMeta> map) {
+            return 0;
+        try{
+            return dataBean.count().intValue();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return 0;
+
+
+
+
+        }
+
+        @Override
+        public List<TipoReserva> load(int i, int i1, Map<String, SortMeta> map, Map<String, FilterMeta> map1) {
+            return List.of();
+        }
+    };
+}
+
+}
